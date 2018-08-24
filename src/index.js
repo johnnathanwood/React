d1eac2f465ce1de451806f316ecfe263a4eca16d
John Wood <johnnathanwood@gmail.com>
2018-08-24 15:44:02 -0500

    working through an error I am getting about module not found

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import EmployeeList from './components/employee/EmployeeList';
 import Kennel from "./components/Kennel"
 
